cc9c38dc409d74c4a0d6b3706f3975cc5f122e08
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-13 02:34:31 +0000

    commented out CA net

--- src/code/model.py
+++ src/code/model.py
@@ -40,35 +40,35 @@ class ResBlock(nn.Module):
         return out
 
 
-class CA_NET(nn.Module):
-    # some code is modified from vae examples
-    # (https://github.com/pytorch/examples/blob/master/vae/main.py)
-    def __init__(self):
-        super(CA_NET, self).__init__()
-        self.t_dim = cfg.TEXT.DIMENSION
-        self.c_dim = cfg.GAN.CONDITION_DIM
-        self.fc = nn.Linear(self.t_dim, self.c_dim * 2, bias=True)
-        self.relu = nn.ReLU()
+# class CA_NET(nn.Module):
+#     # some code is modified from vae examples
+#     # (https://github.com/pytorch/examples/blob/master/vae/main.py)
+#     def __init__(self):
+#         super(CA_NET, self).__init__()
+#         self.t_dim = cfg.TEXT.DIMENSION
+#         self.c_dim = cfg.GAN.CONDITION_DIM
+#         self.fc = nn.Linear(self.t_dim, self.c_dim * 2, bias=True)
+#         self.relu = nn.ReLU()
 
-    def encode(self, text_embedding):
-        x = self.relu(self.fc(text_embedding))
-        mu = x[:, :self.c_dim]
-        logvar = x[:, self.c_dim:]
-        return mu, logvar
+#     def encode(self, text_embedding):
+#         x = self.relu(self.fc(text_embedding))
+#         mu = x[:, :self.c_dim]
+#         logvar = x[:, self.c_dim:]
+#         return mu, logvar
 
-    def reparametrize(self, mu, logvar):
-        std = logvar.mul(0.5).exp_()
-        if cfg.CUDA:
-            eps = torch.cuda.FloatTensor(std.size()).normal_()
-        else:
-            eps = torch.FloatTensor(std.size()).normal_()
-        eps = Variable(eps)
-        return eps.mul(std).add_(mu)
+#     def reparametrize(self, mu, logvar):
+#         std = logvar.mul(0.5).exp_()
+#         if cfg.CUDA:
+#             eps = torch.cuda.FloatTensor(std.size()).normal_()
+#         else:
+#             eps = torch.FloatTensor(std.size()).normal_()
+#         eps = Variable(eps)
+#         return eps.mul(std).add_(mu)
 
-    def forward(self, text_embedding):
-        mu, logvar = self.encode(text_embedding)
-        c_code = self.reparametrize(mu, logvar)
-        return c_code, mu, logvar
+#     def forward(self, text_embedding):
+#         mu, logvar = self.encode(text_embedding)
+#         c_code = self.reparametrize(mu, logvar)
+#         return c_code, mu, logvar
 
 
 class D_GET_LOGITS(nn.Module):
